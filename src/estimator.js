746b34ae7eda3f56f64a8c5d8f53197fb96a77c1
Kamya Domnick <domisemak@yahoo.co.uk>
2020-04-07 20:35:49 +0300

    Completting  challange one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,23 @@
-const covid19ImpactEstimator = (data) => data;
+const input = {
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
+
+const covid19ImpactEstimator = (data) => {
+// Challange 1:
+  const currentlyInfected = data.reportedCases * 10;
+  const severeImpact = data.reportedCases * 50;
+  const infectionsByRequestedTimeForImpact = currentlyInfected * 512;
+  const infectionsByRequestedTimeForSevereImpact = severeImpact * 512;
+
+  return {
+    data: input,
+    impact: infectionsByRequestedTimeForImpact,
+    severeImpact: infectionsByRequestedTimeForSevereImpact
+  };
+};
 
 export default covid19ImpactEstimator;
